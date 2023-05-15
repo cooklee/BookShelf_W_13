eda1caa192003e8434dae3711eda5c3d577b1c49
cooklee <sbbogus@gmail.com>
2023-05-15 10:22:29 +0200

    models

---
+++ ksiazki/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    birth_date = models.DateField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    year = models.IntegerField()
