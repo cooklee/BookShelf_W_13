3a7c5da8871eead917cb92798bde654e95a73fe0
cooklee <sbbogus@gmail.com>
2023-05-15 11:58:30 +0200

    ?

--- ksiazki/urls.py
+++ ksiazki/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 from ksiazki import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('books/', include('ksiazki.urls')),
-    path('', views.IndexView.as_view(), name='Index')
+    path('publishers/', views.PublisherListView.as_view(), name='publisher_list'),
+    path('add_publishers/', views.AddPublisherView.as_view(), name='publisher_add'),
 ]
--- ksiazki/views.py
+++ ksiazki/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'base.html')
+
+
+class PublisherListView(View):
+
+    def get(self, request):
+        return render(request, 'publisher.html')
+
+
+class AddPublisherView(View):
+
+
+    def get(self, request):
+        return render(request, 'add_publisher.html')
