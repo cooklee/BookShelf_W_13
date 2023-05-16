c5f1ce60f6449c1bd80a868c7f09abe18bd07643
cooklee <sbbogus@gmail.com>
2023-05-16 13:02:28 +0200

    get_detail_url

--- book_generic/views.py
+++ book_generic/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from ksiazki.models import Book
+from ksiazki.models import Book, Author
 
 
 # Create your views here.
 
 class BookGenericListView(ListView):
     model = Book
+    template_name = 'list_view.html'
+
+class AuthorGenericListView(ListView):
+    model = Author
     template_name = 'list_view.html'
--- ksiazki/models.py
+++ ksiazki/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
 class Publisher(models.Model):
     name = models.CharField(max_length=128)
+
+    def get_detail_url(self):
+        return reverse('publisher_update', kwargs={'pk': self.id})
 
     def __str__(self):
         return self.name
@@ -12,19 +16,21 @@ class Publisher(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=128)
 
-
-
     def __str__(self):
         return self.name
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     birth_date = models.DateField()
 
-
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    def get_detail_url(self):
+        return reverse('author_update', kwargs={'pk':self.pk})
+
 
 class Book(models.Model):
     title = models.CharField(max_length=128)
@@ -33,6 +39,11 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     year = models.IntegerField()
 
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return self.title
+
+    def get_detail_url(self):
+        return reverse('book_detail', kwargs={'pk':self.pk})
